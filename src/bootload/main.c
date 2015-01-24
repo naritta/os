c99a3c21b12c8cccf3f34c9098ad6c9eec4561a7
RittaNarita <narittan@gmail.com>
2015-01-24 23:26:52 +0900

    add func for interrupt

--- src/bootload/main.c
+++ src/bootload/main.c
@@ -1,4 +1,5 @@
 #include "defines.h"
+#include "interrupt.h"
 #include "serial.h"
 #include "xmodem.h"
 #include "elf.h"
@@ -6,16 +7,26 @@
 
 static int init(void)
 {
+  /* 以下はリンカ・スクリプトで定義してあるシンボル */
   extern int erodata, data_start, edata, bss_start, ebss;
 
+  /*
+   * データ領域とBSS領域を初期化する．この処理以降でないと，
+   * グローバル変数が初期化されていないので注意．
+   */
   memcpy(&data_start, &erodata, (long)&edata - (long)&data_start);
   memset(&bss_start, 0, (long)&ebss - (long)&bss_start);
 
+  /* ソフトウエア・割り込みベクタを初期化する */
+  softvec_init();
+
+  /* シリアルの初期化 */
   serial_init(SERIAL_DEFAULT_DEVICE);
 
   return 0;
 }
 
+/* メモリの16進ダンプ出力 */
 static int dump(char *buf, long size)
 {
   long i;
@@ -52,52 +63,45 @@ int main(void)
   static unsigned char *loadbuf = NULL;
   char *entry_point;
   void (*f)(void);
-  extern int buffer_start;
+  extern int buffer_start; /* リンカ・スクリプトで定義されているバッファ */
 
+  INTR_DISABLE; /* 割込み無効にする */
 
   init();
 
   puts("kzload (kozos boot loader) started.\n");
 
   while (1) {
-    puts("kzload> ");
-    gets(buf);
+    puts("kzload> "); /* プロンプト表示 */
+    gets(buf); /* シリアルからのコマンド受信 */
 
-    if (!strcmp(buf, "load")) {
+    if (!strcmp(buf, "load")) { /* XMODEMでのファイルのダウンロード */
       loadbuf = (char *)(&buffer_start);
       size = xmodem_recv(loadbuf);
-      wait();
-      
+      wait(); /* 転送アプリが終了し端末アプリに制御が戻るまで待ち合わせる */
       if (size < 0) {
-        puts("\nXMODEM receive error!\n");
-        }
-      else {
-        puts("\nXMODEM receive succeeded.\n");
-        }
-    } 
-
-    else if (!strcmp(buf, "dump")) {
+  puts("\nXMODEM receive error!\n");
+      } else {
+  puts("\nXMODEM receive succeeded.\n");
+      }
+    } else if (!strcmp(buf, "dump")) { /* メモリの16進ダンプ出力 */
       puts("size: ");
       putxval(size, 0);
       puts("\n");
       dump(loadbuf, size);
-    }
-
-    else if (!strcmp(buf, "run")) {
-      entry_point = elf_load(loadbuf);
-      if (!entry_point){
-        puts("run error!\n");
+    } else if (!strcmp(buf, "run")) { /* ELF形式ファイルの実行 */
+      entry_point = elf_load(loadbuf); /* メモリ上に展開(ロード) */
+      if (!entry_point) {
+  puts("run error!\n");
       } else {
-        puts("starting from entry point: ");
-        putxval((unsigned long)entry_point, 0);
-        puts("\n");
-        f = (void (*)(void))entry_point;
-        f();
+  puts("starting from entry point: ");
+  putxval((unsigned long)entry_point, 0);
+  puts("\n");
+  f = (void (*)(void))entry_point;
+  f(); /* ここで，ロードしたプログラムに処理を渡す */
+  /* ここには返ってこない */
       }
-
-    }
-
-    else {
+    } else {
       puts("unknown.\n");
     }
   }
--- src/bootload/vector.c
+++ src/bootload/vector.c
@@ -1,6 +1,9 @@
 #include "defines.h"
 
 extern void start(void);
+extern void intr_softerr(void); /* ソフトウエア・エラー */
+extern void intr_syscall(void); /* システム・コール */
+extern void intr_serintr(void); /* シリアル割込み */
 
 /*
  * setting interrupt vector
@@ -8,11 +11,13 @@ extern void start(void);
  */
 void (*vectors[])(void) = {
   start, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 
+  intr_syscall, intr_softerr, intr_softerr, intr_softerr,
   NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
   NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
   NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
   NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
   NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+  intr_serintr, intr_serintr, intr_serintr, intr_serintr,
+  intr_serintr, intr_serintr, intr_serintr, intr_serintr,
+  intr_serintr, intr_serintr, intr_serintr, intr_serintr,
 };
